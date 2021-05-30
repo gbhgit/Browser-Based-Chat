b275e632c2bac54b3265914c2fb1adf95b104e0a
gbhgit <gabrielbandeira@lapisco.ifce.edu.br>
2021-05-30 10:48:22 -0300

    [issue-#3] add create view of room and list all rooms in home

--- chat/chat/settings.py
+++ chat/chat/settings.py
@@ -15,8 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-LOGOUT_REDIRECT_URL = "home"
-LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
--- chat/users/urls.py
+++ chat/users/urls.py
@@ -1,11 +1,11 @@
-# users/urls.py
-
 from django.conf.urls import include, url
-from users.views import home, register, index
+from users.views import register, index, register_room
+
 
 urlpatterns = [
     url(r"^accounts/", include("django.contrib.auth.urls")),
-    url(r"^home/", home, name="home"),
+    #url(r"^home/", home, name="home"),
     url(r"^register/", register, name="register"),
+    url(r"^room/", register_room, name="register_room"),
     url(r"^", index, name="index"),
 ]
