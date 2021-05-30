5ea4a72eaf4c13a660f3862ae36ad5105ea14efb
gbhgit <gabrielbandeira@lapisco.ifce.edu.br>
2021-05-30 17:14:06 -0300

    [issue-#10] add to ws server to recive and send a message from a room

--- chat/users/ws.py
+++ chat/users/ws.py
@@ -1,7 +1,7 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
-from .models import Post
+from .models import Post, ChatRoom, User
 
 class Server(AsyncWebsocketConsumer):
     async def connect(self):
@@ -12,4 +12,28 @@ class Server(AsyncWebsocketConsumer):
     
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group, self.channel_name)
-        
+    
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        await self.save_post(data['user_id'], data['room_id'], data['message'])
+        await self.channel_layer.group_send( self.room_group,
+            {
+                'type': 'send_message',
+                'message': data['message'],
+                'user_id': data['user_id']
+            }
+        )
+    
+    async def send_message(self, event):
+        await self.send(text_data=json.dumps({
+            'message': event['message'],
+            'user_id': event['user_id']
+        }))
+
+    @sync_to_async
+    def save_post(self, user_id, room_id, message):
+        # check if has command in message to create bot
+        room = ChatRoom.objects.get(pk=room_id)
+        user = User.objects.get(pk=user_id)
+        post = Post.objects.create(room=room, msn=message, created_by=user)
+        post.save()
