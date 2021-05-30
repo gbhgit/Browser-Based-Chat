4d6dad068f3954385f7aff956a7fdb049317300e
gbhgit <gabrielbandeira@lapisco.ifce.edu.br>
2021-05-30 08:46:03 -0300

    [issue-#3] add base crud to chat rooms

--- env.sh
+++ env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 virtualenv .env -p python3
 . .env/bin/activate && pip install django
+. .env/bin/activate && pip install djangorestframework
 # python manage.py makemigrations
 # python manage.py migrate
 # python manage.py createsuperuser (admin, admin@gmail.com, admin2020)
