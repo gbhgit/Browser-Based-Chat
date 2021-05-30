4b94fc3aad15082714d2ae32f923ff97449fc15c
gbhgit <gabrielbandeira@lapisco.ifce.edu.br>
2021-05-30 16:42:44 -0300

    [issue-#10] add basic ws router configs

--- chat/chat/settings.py
+++ chat/chat/settings.py
@@ -40,7 +40,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
 ]
+
+WSGI_APPLICATION = 'chat.wsgi.application'
+ASGI_APPLICATION = 'chat.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        }
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
