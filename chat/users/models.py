ea281ad89e36992362c7b1b557579952d9102936
gbhgit <gabrielbandeira@lapisco.ifce.edu.br>
2021-05-30 15:23:48 -0300

    [issue-#9] add post model and show in chat the current username

--- chat/users/models.py
+++ chat/users/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 class ChatRoom(models.Model):
     room_name = models.CharField(max_length=50)
     room_users = models.ManyToManyField(User)
+    
     def __str__(self):
         return self.room_name
+
+class Post(models.Model):
+    room = models.ForeignKey('ChatRoom', on_delete=models.CASCADE)
+    msn = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.msn
