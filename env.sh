afae9346cf9fb0b7275ee2517a70f9a6425431d4
gbhgit <gabrielbandeira@lapisco.ifce.edu.br>
2021-05-31 11:22:00 -0300

    [issue-#23] add tests using django.test

--- env.sh
+++ env.sh
@@ -4,6 +4,7 @@ virtualenv .env -p python3
 . .env/bin/activate && pip install djangorestframework
 . .env/bin/activate && pip install channels
 . .env/bin/activate && pip install channels_redis
+. .env/bin/activate && pip install pytest
 # python manage.py makemigrations
 # python manage.py migrate
 # python manage.py createsuperuser (admin, admin@gmail.com, admin2020)
